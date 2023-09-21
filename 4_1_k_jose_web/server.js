bac9001747217c54a5315a494f989e0265ea2577
Keizan Jose <keizanjose30@gmail.com>
2023-09-21 20:59:42 +0800

    added initial login page ui

--- 4_1_k_jose_web/server.js
+++ 4_1_k_jose_web/server.js
@@ -3,9 +3,8 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-})
+app.use(express.static("public"));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
